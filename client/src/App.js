b068c416160f2ca78f72daa574088c58bd104f80
DavinLee <davin11992@gmail.com>
2024-08-24 02:24:04 +0900

    회원가입 편비봉투가 뒤로 안감

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
